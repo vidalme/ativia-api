f10fe5f4087cdc49b9bea9c545a31c6b472317c5
Andre Almeida <andre.almeida@hp.com>
2024-10-09 03:07:00 -0300

    Modify - adress of the container with the postgresql

--- internal/db/db.go
+++ internal/db/db.go
@@ -7,7 +7,10 @@ import (
 )
 
 func ConectaComBancoDeDados() *sql.DB {
-	conexao := "host=172.18.0.2 port=5432 user=admin password=qwe dbname=postgres sslmode=disable "
+	// usando temporariamente os ips absolutos por que estou compilando o codigo fora do docker compose,
+	// entao fora da network sem possibilidade de usar o DNS interno do compose
+	// conexao := "host=172.18.0.2 port=5432 user=admin password=qwe dbname=postgres sslmode=disable "
+	conexao := "host=172.19.0.2 port=5432 user=admin password=qwe dbname=postgres sslmode=disable "
 	// connStr := "user=postgres password=yourpassword dbname=yourdbname sslmode=disable"
 	db, err := sql.Open("postgres", conexao)
 	if err != nil {
