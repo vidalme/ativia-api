8954af28a9f322fb594b8fa90029739fab92bb78
Andre Almeida <andre.almeida@hp.com>
2024-10-09 09:00:19 -0300

    Organize named javascripts correctly

---
+++ static/js/updateUser.js
@@ -0,0 +1,34 @@
+function onUpdate() {
+    // Collect form data
+    const formData = {
+        id: document.getElementById('id').value,
+        userName: document.getElementById('user_name').value,
+        firstName: document.getElementById('first_name').value,
+        lastName: document.getElementById('last_name').value,
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value,
+        phone: document.getElementById('phone').value,
+        suerStatus: document.getElementById('user_status').value
+    };
+
+    // Perform a PUT request using fetch
+    fetch('/user', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData) // Send the form data as JSON
+    })
+    .then(response => {
+        if (response.ok) {
+            alert('Usuário atualizado com sucesso!');
+            window.location.href = '/'; // Redirect after success
+        } else {
+            alert('Erro ao atualizar usuário.');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Ocorreu um erro ao tentar atualizar o usuário.');
+    });
+}
